50f51c457f4d484369992202c7b0fdff84e0f581
albertomarin <albertomarinmorales@gmail.com>
2025-02-23 10:11:01 +0100

    Borrado srcipt del HTML y pasado al archivo script.js

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-console.log('Trabajando en la rama main');
+    let getLetter = () =>{
+        let  letter = [' a ',' b ',' c ',' d ',' e ',' f ',' g ',' h ',' i ',' j ',' k ',' l ',' m ',' n ',' o ',' p ',' q ',' r ',' s ',' t ',' u ',' v ',' w ',' x ',' y ',' z '];
+        let takeLetter = '';
+        for (let i = 0; i < 1350; i++) {
+           switch (i){
+            case 600:
+                takeLetter += `<div class="showName" id=$div${i}>ALBERTO</div>`;
+                break;
+            }
+            takeLetter += `<div class="letter" id=$div${i}>${letter[Math.floor(Math.random() * 26)]}</div>`;
+        }
+        return takeLetter;
+    }
+    document.getElementsByTagName('header')[0].innerHTML = getLetter();
+let letters = document.querySelectorAll('.letter');     
+console.log(letters.length)
 
-let variable1= 'Probando stash';
-let variable2= 'probando stash 2';
+function lightUpLetter() {
+    let randomLetter = Math.floor(Math.random() * letters.length);
+    if (randomLetter % 2 == 0){
+        letters[randomLetter].classList.add('letterPurple');
+        setTimeout(() => {
+        letters[randomLetter].classList.add('letterOff');
+    }, 3000);
+    }else{
+        letters[randomLetter].classList.add('letterBlue');
+        setTimeout(() => {
+        letters[randomLetter].classList.add('letterOff');
+    }, 3000);
+    }
+}
+
+setInterval(lightUpLetter, 500);
